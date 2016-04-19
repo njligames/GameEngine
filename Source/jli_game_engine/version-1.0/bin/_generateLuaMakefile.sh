0c025cd0010fc3a83a6f3a8d253b6e850119c2ed
james_folk <jamesfolk1@gmail.com>
2016-04-19 17:40:31 -0400

    fixed the make file generator script
    Former-commit-id: 1972d52c7978d1934c169c9455df01d162e54152
    Former-commit-id: 4153c3a9cc8dd506ef0c31ba2c598cf5cd0550d1

--- Source/jli_game_engine/version-1.0/bin/_generateLuaMakefile.sh
+++ Source/jli_game_engine/version-1.0/bin/_generateLuaMakefile.sh
@@ -1,6 +1,5 @@
 GAMEPLAYSCRIPTS_ABSOLUTEPATH=${1}
 
-# _GAMEPLAYSCRIPTS=./
 _SCRIPTS=/../assets/scripts
 
 SCRIPTS_ABSOLUTEPATH=${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${_SCRIPTS}
@@ -10,20 +9,19 @@ SCRIPTS=${GAMEPLAYSCRIPTS}/../assets/scripts
 
 rm -rf ${SCRIPTS_ABSOLUTEPATH}/*
 
->&2 echo "make the directories..."
+TARGET='all: '
+
+>&2 echo "accumulate the directory targets..."
 for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type d -not -path "*_OLD*" -not -path "*_archive*")
 do
   if [ "${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}" != "" ]
   then
-    mkdir ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}
+    TARGET="${TARGET} ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}"
   fi
 done
 
 
->&2 echo "list all the targets..."
-
-TARGET='all: '
-
+>&2 echo "accumulate the file targets..."
 for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type f -name "*.lua" -not -path "*/_OLD/*" -not -path "*/_archive/*" -not -path "*/_*" )
 do
     FILE=${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}
@@ -38,17 +36,25 @@ echo ''
 
 
 
->&2 echo "make all the targets..."
-#make all the targets...
+>&2 echo "list the file targets..."
 for i in $(find $GAMEPLAYSCRIPTS_ABSOLUTEPATH -type f -name "*.lua" -not -path "*/_OLD/*" -not -path "*/_archive/*" -not -path "*/_*" )
 do
-    FILE=${i##$GAMEPLAYSCRIPTS_ABSOLUTEPATH}
+  FILE=${i##$GAMEPLAYSCRIPTS_ABSOLUTEPATH}
 
-    echo ${SCRIPTS}${FILE}: ${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${FILE}
-    echo '\tperl ./../bin/removeComments.pl' '<' ${GAMEPLAYSCRIPTS}${FILE} '>' ${SCRIPTS}${FILE}
+  echo ${SCRIPTS}${FILE}: ${GAMEPLAYSCRIPTS}${FILE}
+  echo '\tperl ./../bin/removeComments.pl' '<' ${GAMEPLAYSCRIPTS}${FILE} '>' ${SCRIPTS}${FILE}
+  echo ''
+done
+
+>&2 echo "list the directory targets..."
+for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type d -not -path "*_OLD*" -not -path "*_archive*")
+do
+  if [ "${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}" != "" ]
+  then
+    echo ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}:
+    echo '\tmkdir -p ' ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}
     echo ''
+  fi
 done
 
 >&2 echo "done..."
-
-
