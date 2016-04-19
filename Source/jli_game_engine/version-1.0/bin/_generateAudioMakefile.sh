8b775684aebfa8e3fd785a948c60eb8d5fad13ef
james_folk <jamesfolk1@gmail.com>
2016-04-19 13:09:02 -0400

    fixed the audio generations script to use relative paths
    Former-commit-id: a5b968ed04cf2e5462406db2d5138f77fd9aebf8
    Former-commit-id: 228fa99cda852ee22e75f539adc5d75daafa96e2

--- Source/jli_game_engine/version-1.0/bin/_generateAudioMakefile.sh
+++ Source/jli_game_engine/version-1.0/bin/_generateAudioMakefile.sh
@@ -1,46 +1,119 @@
-GAMEPLAY_SCRIPTS=$1
-SCRIPTS=$1/../assets/sounds
-EXE=$2
+GAMEPLAYSCRIPTS_ABSOLUTEPATH=${1}
 
-rm -rf ${SCRIPTS}/*
+# _GAMEPLAYSCRIPTS=./
+_SCRIPTS=/../assets/sounds
 
+SCRIPTS_ABSOLUTEPATH=${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${_SCRIPTS}
+
+GAMEPLAYSCRIPTS=.
+SCRIPTS=${GAMEPLAYSCRIPTS}/../assets/sounds
+
+# rm -rf ${SCRIPTS_ABSOLUTEPATH}/*
+
+# >&2 echo "make the directories..."
+# for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type d -not -path "*_OLD*" -not -path "*_archive*")
+# do
+#   if [ "${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}" != "" ]
+#   then
+#     mkdir ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}
+#   fi
+# done
+
+
+# >&2 echo "list all the targets..."
+
+# TARGET='all: '
+
+# for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type f -name "*.lua" -not -path "*/_OLD/*" -not -path "*/_archive/*" -not -path "*/_*" )
+# do
+#     FILE=${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}
+#     FULLPATH=${SCRIPTS}${FILE}
+#     TARGET="${TARGET} ${FULLPATH}"
+# done
+
+# echo ${TARGET}
+# echo ''
+
+
+
+
+
+# >&2 echo "make all the targets..."
+# #make all the targets...
+# for i in $(find $GAMEPLAYSCRIPTS_ABSOLUTEPATH -type f -name "*.lua" -not -path "*/_OLD/*" -not -path "*/_archive/*" -not -path "*/_*" )
+# do
+#     FILE=${i##$GAMEPLAYSCRIPTS_ABSOLUTEPATH}
+
+#     echo ${SCRIPTS}${FILE}: ${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${FILE}
+#     echo '\tperl ./../bin/removeComments.pl' '<' ${GAMEPLAYSCRIPTS}${FILE} '>' ${SCRIPTS}${FILE}
+#     echo ''
+# done
+
+# >&2 echo "done..."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # GAMEPLAY_SCRIPTS=$1
+# # SCRIPTS=$1/../assets/sounds
+# # EXE=$2
+
+rm -rf ${SCRIPTS_ABSOLUTEPATH}/*
+
+>&2 echo "make the directories..."
 #make the directories.
-for i in $(find ${GAMEPLAY_SCRIPTS} -type d -not -path "*audacity*" -not -path "*hydrogen*" -not -path "*/_*")
+for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type d -not -path "*audacity*" -not -path "*hydrogen*" -not -path "*/_*")
 do
-	if [ "${i##${GAMEPLAY_SCRIPTS}}" != "" ]
+	if [ "${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}" != "" ]
 	then
-		mkdir ${SCRIPTS}/${i##${GAMEPLAY_SCRIPTS}/}
+		mkdir ${SCRIPTS_ABSOLUTEPATH}/${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/}
 	fi
 done
 
 WAV=wav
 OGG=ogg
 
+>&2 echo "list all the targets..."
 #list all the targets...
 TARGET='all: '
 
-for i in $(find ${GAMEPLAY_SCRIPTS} -type f -name "*.wav" -not -path "*/audacity/*" -not -path "*/hydrogen/*" -not -path "*/_*" )
+for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type f -name "*.wav" -not -path "*/audacity/*" -not -path "*/hydrogen/*" -not -path "*/_*" )
 do
 
-    FILE=${i##${GAMEPLAY_SCRIPTS}}
-	TO=${SCRIPTS}${FILE}
-	FULLPATH=${TO%.${WAV}}.${OGG}
+  FILE=${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}
+  TO=${SCRIPTS}${FILE}
+  FULLPATH=${TO%.${WAV}}.${OGG}
 
-    TARGET="${TARGET} ${FULLPATH}"
+  TARGET="${TARGET} ${FULLPATH}"
 done
 
 echo $TARGET
 echo ''
 
-
+>&2 echo "make all the targets..."
 #make all the targets...
-for i in $(find ${GAMEPLAY_SCRIPTS} -type f -name "*.wav" -not -path "*/audacity/*" -not -path "*/hydrogen/*" -not -path "*/_*" )
+for i in $(find ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} -type f -name "*.wav" -not -path "*/audacity/*" -not -path "*/hydrogen/*" -not -path "*/_*" )
 do
-    FILE=${i##${GAMEPLAY_SCRIPTS}}
-	TO=${SCRIPTS}${FILE}
-	FULLPATH=${TO%.${WAV}}.${OGG}
+  FILE=${i##${GAMEPLAYSCRIPTS_ABSOLUTEPATH}}
+  TO=${SCRIPTS}${FILE}
+  FULLPATH=${TO%.${WAV}}.${OGG}
 
-    echo ${FULLPATH}: ${GAMEPLAY_SCRIPTS}${FILE}
-	echo '\t'${EXE} ${GAMEPLAY_SCRIPTS}${FILE} ${FULLPATH}
-    echo ''
+  echo ${FULLPATH}: ${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${FILE}
+  echo '\t./../bin/sox' ${GAMEPLAYSCRIPTS_ABSOLUTEPATH}${FILE} ${FULLPATH}
+  echo ''
 done
--- Source/jli_game_engine/version-1.0/bin/generateAudioMakefile.sh
+++ Source/jli_game_engine/version-1.0/bin/generateAudioMakefile.sh
@@ -1,8 +1,21 @@
 #PROJECT=GameEngine
-PROJECT=YappyBirds
+_PROJECT=YappyBirds
 
-SOURCE=/Users/jamesfolk/NJLI/${PROJECT}/COMMON/audio
-pushd /Users/jamesfolk/NJLI/GameEngine/Source/jli_game_engine/version-1.0/bin
-./_generateAudioMakefile ${SOURCE} /Users/jamesfolk/NJLI/${PROJECT}/COMMON/bin/sox > ${SOURCE}/Makefile
-popd
+_PROJECT_PATH=/Users/jamesfolk/NJLI/${_PROJECT}
+
+GAMEPLAYSCRIPTS_ABSOLUTEPATH=${_PROJECT_PATH}/COMMON/audio
+
+./_generateAudioMakefile.sh ${GAMEPLAYSCRIPTS_ABSOLUTEPATH} > ${GAMEPLAYSCRIPTS_ABSOLUTEPATH}/Makefile
+
+
+
+
+
+#PROJECT=GameEngine
+# PROJECT=YappyBirds
+
+# SOURCE=/Users/jamesfolk/NJLI/${PROJECT}/COMMON/audio
+# pushd /Users/jamesfolk/NJLI/GameEngine/Source/jli_game_engine/version-1.0/bin
+# ./_generateAudioMakefile ${SOURCE} /Users/jamesfolk/NJLI/${PROJECT}/COMMON/bin/sox > ${SOURCE}/Makefile
+# popd
 
